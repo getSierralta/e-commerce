ba5ab0f51ffa818ff796c1f27991e3d1f15c1234
getsierralta <sierraltamaximiliano8@gmail.com>
2021-02-10 17:05:52 +0000

    React app

---
+++ frontend/src/data.js
@@ -0,0 +1,114 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'Laptop',
+            category: 'Computers', 
+            image: './img/p1.jpg', 
+            price: 360,
+            brand: 'Generic',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality'
+        },
+        {
+            _id: '2',
+            name: 'Tablet',
+            category: 'Tablets', 
+            image: './img/p2.jpg', 
+            price: 360,
+            brand: 'Generic',
+            rating: 4,
+            numReviews: 6,
+            description: 'high quality'
+        },
+        {
+            _id: '3',
+            name: 'Phone',
+            category: 'Phones', 
+            image: './img/p3.jpg', 
+            price: 360,
+            brand: 'Generic',
+            rating: 4,
+            numReviews: 5,
+            description: 'high quality'
+        },
+        {
+            _id: '4',
+            name: 'Oculus',
+            category: 'VideoGames', 
+            image: './img/p4.jpg', 
+            price: 360,
+            brand: 'Generic',
+            rating: 3,
+            numReviews: 2,
+            description: 'high quality'
+        },
+        {
+            _id: '5',
+            name: 'CPU',
+            category: 'Computers', 
+            image: './img/p5.jpg', 
+            price: 360,
+            brand: 'Generic',
+            rating: 5,
+            numReviews: 17,
+            description: 'high quality'
+        },
+        {
+            _id: '6',
+            name: 'Lense',
+            category: 'Cameras', 
+            image: './img/p6.jpg', 
+            price: 360,
+            brand: 'Generic',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality'
+        },
+        {
+            _id: '7',
+            name: 'Camera',
+            category: 'Cameras', 
+            image: './img/p7.jpg', 
+            price: 360,
+            brand: 'Generic',
+            rating: 4,
+            numReviews: 6,
+            description: 'high quality'
+        },
+        {
+            _id: '8',
+            name: 'Laptop',
+            category: 'Computers', 
+            image: './img/p8.jpg', 
+            price: 360,
+            brand: 'Generic',
+            rating: 4,
+            numReviews: 5,
+            description: 'high quality'
+        },
+        {
+            _id: '9',
+            name: 'Switch',
+            category: 'VideoGames', 
+            image: './img/p4.jpg', 
+            price: 360,
+            brand: 'Generic',
+            rating: 5,
+            numReviews: 2,
+            description: 'high quality'
+        },
+        {
+            _id: '10',
+            name: 'Phone',
+            category: 'Phones', 
+            image: './img/p10.jpg', 
+            price: 360,
+            brand: 'Generic',
+            rating: 5,
+            numReviews: 17,
+            description: 'high quality'
+        },
+    ]
+}
