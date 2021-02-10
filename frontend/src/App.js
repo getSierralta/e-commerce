7240868bdfec487eb2f55b5886083c0face32d7c
getsierralta <sierraltamaximiliano8@gmail.com>
2021-02-10 19:34:47 +0000

    Day 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,90 +1,31 @@
-
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="grid-container">
         <header className="row">
             <div>
-                <a className="title" href="index.html">E-Tech</a>
+                <a className="title" href="/">E-Tech</a>
             </div>
             <div>
-                <a href="cart.html">Cart</a>
-                <a href="signin.html">Sing In</a>
+                <a href="/cart">Cart</a>
+                <a href="/signin">Sing In</a>
             </div>
         </header>
         <main>
-            <div className="row center">
-                <div className="card">
-                    <a href="product.html"><img className="medium" src="./img/p1.jpg" alt="Computer"></img></a>
-                    <div className="card-body">
-                        <a href="product.html">
-                            <h2>Laptop</h2>
-                        </a>
-                        <div className="rating">
-                            <span><i className="fa fa-star"></i></span>
-                            <span><i className="fa fa-star"></i></span>
-                            <span><i className="fa fa-star"></i></span>
-                            <span><i className="fa fa-star"></i></span>
-                            <span><i className="fa fa-star"></i></span>
-                        </div>
-                        <div className="price">$360</div>
-                    </div>
-                </div>
-                <div className="card">
-                    <a href="product.html"><img className="medium" src="./img/p3.jpg" alt="phone"></img></a>
-                    <div className="card-body">
-                        <a href="product.html">
-                            <h2>Phone</h2>
-                        </a>
-                        <div className="rating">
-                            <span><i className="fa fa-star"></i></span>
-                            <span><i className="fa fa-star"></i></span>
-                            <span><i className="fa fa-star"></i></span>
-                            <span><i className="fa fa-star"></i></span>
-                            <span><i className="fa fa-star"></i></span>
-                        </div>
-                        <div className="price">$360</div>
-                    </div>
-                </div>
-                <div className="card">
-                    <a href="product.html"><img className="medium" src="./img/p4.jpg" alt="oculus"></img></a>
-                    <div className="card-body">
-                        <a href="product.html">
-                            <h2>Oculus</h2>
-                        </a>
-                        <div className="rating">
-                            <span><i className="fa fa-star"></i></span>
-                            <span><i className="fa fa-star"></i></span>
-                            <span><i className="fa fa-star"></i></span>
-                            <span><i className="fa fa-star"></i></span>
-                            <span><i className="fa fa-star"></i></span>
-                        </div>
-                        <div className="price">$360</div>
-                    </div>
-                </div>
-                <div className="card">
-                    <a href="product.html"><img className="medium" src="./img/p5.jpg" alt="CPU"></img></a>
-                    <div className="card-body">
-                        <a href="product.html">
-                            <h2>CPU</h2>
-                        </a>
-                        <div className="rating">
-                            <span><i className="fa fa-star"></i></span>
-                            <span><i className="fa fa-star"></i></span>
-                            <span><i className="fa fa-star"></i></span>
-                            <span><i className="fa fa-star"></i></span>
-                            <span><i className="fa fa-star"></i></span>
-                        </div>
-                        <div className="price">$360</div>
-                    </div>
-                </div>
-            </div>
+            <Route path="/product/:id" component = {ProductScreen}></Route>
+            <Route path="/" component = {HomeScreen} exact></Route>
         </main>
         <footer className="row center">
             <p>All rights reserved</p>
         </footer>
     </div>
+    </BrowserRouter>
   );
 }
 
